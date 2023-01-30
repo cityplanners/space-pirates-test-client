bae91bb1bd42f6a15d2fccb7cdfad437490c72f9
cpadilla <christoferpadilla@gmail.com>
2023-01-29 23:54:09 -0800

    Added chat stream to read from server

--- build.rs
+++ build.rs
@@ -1,4 +1,13 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/helloworld.proto")?;
+
+    let mut attributes = tonic_build::Attributes::default();
+    attributes.push_struct("ChatReqClient", r#"#[derive(Resource)]"#);
+
+    tonic_build::configure()
+        .build_server(false)
+        .compile(&["proto/chat.proto"],
+            &["proto".to_string()]
+        )?;
+    
     Ok(())
 }
